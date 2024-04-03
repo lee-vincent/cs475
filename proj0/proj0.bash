79e014b0ab0a2df278da35855093bd8e299668b8
lee-vincent <vinnie@vinnielee.io>
2024-04-03 06:48:36 -0400

    add proj1

--- proj0/proj0.bash
+++ proj0/proj0.bash
@@ -2,8 +2,8 @@
 
 #number of threads:
 for t in 1 4
-do
-echo NUMT = $t
-g++ -DNUMT=$t proj0.cpp -o proj0 -lm -fopenmp
-./proj0
+    do
+        echo NUMT = $t
+        g++ -DNUMT=$t proj0.cpp -o proj0 -lm -fopenmp
+        ./proj0
 done
