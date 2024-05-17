78771d191312a16e5242d1bf0be15cda6c915bdc
lee-vincent <vinnie@vinnielee.io>
2024-05-17 12:26:25 -0400

    fix bash scripts

--- proj6/proj6.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for s in 4096 16384 65536 262144 1048576 4194304
-do
-    for b in 8 32 64 128 256
-    do
-            g++ -DDATASIZE=$s -DLOCALSIZE=$b -o proj06 proj06.cpp /usr/local/apps/cuda/11.7/lib64/libOpenCL.so.1  -lm -fopenmp
-            ./proj06
-    done
-done
